8c5315a4b54df6ee7151abb2d4302318f3157bd2
sulbin <tlsdnr1135@naver.com>
2023-03-08 01:08:25 +0900

    no message

--- src/main/java/com/example/week8/entity/Agroup.java
+++ src/main/java/com/example/week8/entity/Agroup.java
@@ -1,14 +1,17 @@
 package com.example.week8.entity;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 @Entity(name = "AGROUP")
-public class Agroup {
+public class Agroup extends Timestamped{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,9 +19,17 @@ public class Agroup {
     private Long id;
 
     private String agroupName;
+
     @CreatedDate
     private LocalDateTime regTime;
+
     private Integer agroupActYn;
 
-
+    @Builder
+    public Agroup(Long id, String agroupName, LocalDateTime regTime, Integer agroupActYn) {
+        this.id = id;
+        this.agroupName = agroupName;
+        this.regTime = regTime;
+        this.agroupActYn = agroupActYn;
+    }
 }

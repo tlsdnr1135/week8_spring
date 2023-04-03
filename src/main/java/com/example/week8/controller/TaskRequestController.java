854e05c419bb7291f4b38498c0afc181fc1e8701
sulbin <tlsdnr1135@naver.com>
2023-04-04 02:31:51 +0900

    no message

--- src/main/java/com/example/week8/controller/TaskRequestController.java
+++ src/main/java/com/example/week8/controller/TaskRequestController.java
@@ -3,10 +3,11 @@ package com.example.week8.controller;
 import com.example.week8.service.TaskRequestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +16,13 @@ public class TaskRequestController {
     private final TaskRequestService taskRequestService;
     //파일 저장
     @PostMapping("/api/v1/files")
-    public ResponseEntity<?> saveFiles(@RequestBody MultipartFile formData){
+    public ResponseEntity<?> saveFiles(@RequestParam("formData") MultipartFile formData) throws IOException {
         System.out.println(formData);
+        System.out.println(formData.getOriginalFilename());
+        System.out.println(formData.getName());
+        System.out.println(formData.getResource());
+        formData.transferTo(new File("C:\\java_workspace\\IntelliJ_workspace"));
+//        System.out.println(formData.getResource());
         taskRequestService.saveFiles();
         return ResponseEntity.ok().body(null);
     }
